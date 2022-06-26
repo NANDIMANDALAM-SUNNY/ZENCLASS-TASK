645c45aadb19165f9f873468f97af224e5fc655d
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-06-26 10:13:13 +0530

    Add files via upload

---
+++ 14-05-2022-Day-9/Task-1/index.js
@@ -0,0 +1,12 @@
+// create a class named test and add a method named test
+// add a method named test2
+
+
+class Test {
+    test() {
+        console.log('test');
+    }
+    test2() {
+        console.log('test2');
+    }
+}
