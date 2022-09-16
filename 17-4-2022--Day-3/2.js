bd409ee302271f00e1bb293c29495266d7ee2882
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-09-17 02:16:46 +0530

    Add files via upload

---
+++ 17-4-2022--Day-3/2.js
@@ -0,0 +1,56 @@
+const myResume={
+    "basics": {
+      "name": "NANDIMANDALAM SUNNY",
+      "email": "r170911@rguktrkv.ac.in",
+      "phone": 8008726523,
+      "degree": "B-Tech",
+      "location": {
+        "address": "Vempalle",
+        "postalCode": 516329,
+        "city": "Kadapa",
+        "state": "Andhra Pradesh",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "http://nandimandalamsunny.netlify.app/",
+          "github":"https://github.com/NANDIMANDALAM-SUNNY"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Suven Internships Private Limited",
+        "position": "Intern",
+        "startDate": "02/2022",
+        "endDate": "02/2022,",
+        "summary": "i am a junior javascript developer",
+      },
+    ],
+    "education": [
+      {
+        "institution": "RGUKT",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2023,
+        "gpa": 7.81,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "intermediate",
+        "project": [
+          "Esculea,CryptoBase"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Telugu,Enlish,Hindi",
+      }
+    ],
+
+  }
+  console.log(myResume);
