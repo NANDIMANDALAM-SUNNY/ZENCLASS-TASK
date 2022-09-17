8fbb883a97cb2da17ef12ceda0b41fe9f9bf4bfb
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-09-17 15:52:47 +0530

    Add files via upload

---
+++ 21-08-2033--Day-29/imdb.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE imdb (
+-- 	id int NOT NULL AUTO_INCREMENT ,
+--     music varchar(255),
+--     video varchar(255),
+--     genre varchar(255),
+--     review int,
+--     skills varchar(255),
+--     PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO imdb(music,video,genre,review,skills)
+-- VALUES 
+-- ('music2','video2','action',1,'fighting'),
+-- ('music3','video3','horror',1,'acting'),
+-- ('music4','video4','horror',4,'dancing');
+
+select * from imdb
