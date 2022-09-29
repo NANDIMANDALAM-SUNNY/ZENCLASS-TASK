3c8da6235861ad0063f78f536db9f07d465f7448
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-09-29 10:48:54 +0530

    Delete 18-09-2022--Day-35/backend directory

--- 18-09-2022--Day-35/backend/dbConnection.js
+++
@@ -1,6 +0,0 @@
-const mongodb = require('mongodb')
-const dbName = "StudentMentor"
-const dburl ="mongodb+srv://sunny:sunny@cluster0.ck6j4.mongodb.net/?retryWrites=true&w=majority"
-const mongoClient = mongodb.MongoClient
-
-module.exports = {mongodb,dbName,dburl,mongoClient}
