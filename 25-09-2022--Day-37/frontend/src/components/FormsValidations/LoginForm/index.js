1dc5ccc2074f6a0823ec6c383d0493a21225adf1
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-10-31 01:40:38 +0530

    Delete 25-09-2022--Day-37 directory

--- 25-09-2022--Day-37/frontend/src/components/FormsValidations/LoginForm/index.js
+++
@@ -1,6 +0,0 @@
-import * as Yup from 'yup'
-
-export const loginSchema = Yup.object({
-    email:Yup.string().email().required("Please Enter Your Email"),
-    password:Yup.string().min(2).max(10).required("Please Enter Your Password"),
-})
--- 25-09-2022--Day-37/frontend/src/components/FormsValidations/SignupForm/index.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup'
-
-export const signUpSchema = Yup.object({
-   name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Name Required'),
-   email: Yup.string().email('Invalid email').required('Email Required'),
-   mobileNumber: Yup.string().min(2, 'Too Short!').max(11, 'Too Long!').required('Mobile NumberRequired'),
-   role: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Role Required'),
-   password: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Password Required'),
-})
